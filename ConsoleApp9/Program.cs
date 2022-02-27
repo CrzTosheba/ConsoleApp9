557f65dd42c12f4e5b5a2e948df3b1799a57697c
CrzTosheba <fugu@list.ru>
2022-02-27 08:41:12 +0300

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class Program
+{
+        static void Main(string[] args)
+    {
+        static (string Name,  string LastName, int Age) EnterUser()
+        {
+            (string Name, string LastName, int Age) User;
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию");
+            User.LastName = Console.ReadLine();
+            
+            
+            Console.WriteLine("Введите возраст цифрами");
+            User.Age = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+            /*Console.WriteLine("У вас есть питомец?");
+            User.HavePet = Console.ReadLine();*/
+
+
+
+
+            return User;
+        }
+        EnterUser();
+        
+        
+    }
+   
+ 
+    
+
+
+
+    
+}
